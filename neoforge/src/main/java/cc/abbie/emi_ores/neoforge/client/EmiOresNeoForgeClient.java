68e4b88c8278df06910529fb85def4ec9899ac72
Abbie <abigail@ient.me>
2025-01-05 17:06:06 +0000

    use client-only @Mod annotation instead of client setup event for neoforge client entrypoint

--- neoforge/src/main/java/cc/abbie/emi_ores/neoforge/client/EmiOresNeoForgeClient.java
+++ neoforge/src/main/java/cc/abbie/emi_ores/neoforge/client/EmiOresNeoForgeClient.java
@@ -1,21 +1,19 @@
 package cc.abbie.emi_ores.neoforge.client;
 
-import cc.abbie.emi_ores.EmiOres;
-import cc.abbie.emi_ores.client.EmiOresClient;
-import cc.abbie.emi_ores.client.FeaturesReciever;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
 import net.neoforged.neoforge.common.NeoForge;
 
-import static net.neoforged.fml.common.EventBusSubscriber.*;
+import cc.abbie.emi_ores.EmiOres;
+import cc.abbie.emi_ores.client.EmiOresClient;
+import cc.abbie.emi_ores.client.FeaturesReciever;
 
-@EventBusSubscriber(modid = EmiOres.MODID, bus = Bus.MOD, value = Dist.CLIENT)
+@Mod(value = EmiOres.MODID, dist = Dist.CLIENT)
 public class EmiOresNeoForgeClient {
-    @SubscribeEvent
-    public static void init(FMLClientSetupEvent event) {
+    public EmiOresNeoForgeClient(ModContainer mod, IEventBus modBus) {
         EmiOresClient.init();
 
         NeoForge.EVENT_BUS.addListener((ClientPlayerNetworkEvent.LoggingOut e) -> FeaturesReciever.clearFeatures());
